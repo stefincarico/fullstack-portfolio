bb91ba7462a0784129baa82000652c5d6c42111f
Stefano Incarico <stefano.incarico@outlook.it>
2025-09-19 19:11:07 +0200

    feat: Create homepage view and UR

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+# pages/views.py
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page_view(request):
+    return HttpResponse("<h1>Ciao Mondo! Questa è la homepage.</h1>")
