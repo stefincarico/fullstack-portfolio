42b5b7d97247dd88c7583d1900d61a5307a5c703
Stefano Incarico <stefano.incarico@outlook.it>
2025-09-20 13:29:13 +0200

    style: Integrate Bootstrap and refactor templates

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'accounts',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
