93e6142bf9dd41b0711f22fb43c073aff34927d1
Stefano Incarico <stefano.incarico@outlook.it>
2025-09-20 15:04:50 +0200

    feat: REST API post detail view

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import PostListApiView
+from .views import PostListApiView, PostDetailApiView
 
 urlpatterns = [
     path('posts/', PostListApiView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailApiView.as_view(), name='post-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from blog.models import Post
 from .serializers import PostSerializer
 
@@ -12,4 +13,15 @@ class PostListApiView(APIView):
         '''
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class PostDetailApiView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        '''
+        Restituisce i dettagli di un singolo post.
+        '''
+        # 1. Recupera il singolo oggetto usando la pk e get_object_or_404
+        post = get_object_or_404(Post, pk=pk)
+        # 2. Serializza il singolo oggetto (NON serve many=True)
+        serializer = PostSerializer(post)
         return Response(serializer.data, status=status.HTTP_200_OK)
